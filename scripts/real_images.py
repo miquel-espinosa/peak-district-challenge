3d21b2bcdafe2ed648adf6388849ae1eb0178c2d
Miguel Espinosa <miguel.espinosa@SRE-DSGPEAK-180.prod4.turingsafehaven.ac.uk>
2023-05-25 20:31:24 +0100

    realimages

--- scripts/real_images.py
+++ scripts/real_images.py
@@ -32,6 +32,9 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
 args = parser.parse_args()
 
+PLOT=False
+SAVE_FIGS=True
+SAVE_PATH_FIG='data/example_tiles_complete/12.5cm-Aerial-Photo/change-tiles'
 
 def compute_loss_and_acc(loader, model, criterion, subset):
     loss = 0
@@ -73,12 +76,12 @@ SEED=args.seed
 
 # ROOT_PATH = '/home/s2254242/PHD/ATI/peak-district-challenge'
 ROOT_PATH = '/shared/miguel/peak-district-challenge'
-dir_test_im_patches = f'{ROOT_PATH}/data/images_detailed_annotation/'
-dir_test_mask_patches = f'{ROOT_PATH}/data/masks_detailed_annotation/'
-path_mapping_dict=f'{ROOT_PATH}/content/label_mapping_dicts/label_mapping_dict__main_categories__2023-04-20-1541.pkl'
+# dir_test_im_patches = f'{ROOT_PATH}/data/images_detailed_annotation/'
+# dir_test_mask_patches = f'{ROOT_PATH}/data/masks_detailed_annotation/'
+# path_mapping_dict=f'{ROOT_PATH}/content/label_mapping_dicts/label_mapping_dict__main_categories__2023-04-20-1541.pkl'
 
-mask_suffix_test_ds = '_lc_2022_detailed_mask.npy'
-mask_dir_name_test = ''
+# mask_suffix_test_ds = '_lc_2022_detailed_mask.npy'
+# mask_dir_name_test = ''
 
 
 
@@ -86,126 +89,154 @@ mask_dir_name_test = ''
 
 # TODO: Dataset train, val, test split
 # Define dataset
-ds = lcm.DataSetPatches(im_dir=dir_test_im_patches, mask_dir=dir_test_mask_patches, 
-                                    mask_suffix=mask_suffix_test_ds, mask_dir_name=mask_dir_name_test,
-                                #   list_tile_names=dict_tile_names_sample['test'],
-                                    list_tile_patches_use=None,
-                                    shuffle_order_patches=True, relabel_masks=True,
-                                    random_transform_data=True,
-                                    subsample_patches=False, # frac_subsample=0.1,
-                                    path_mapping_dict=path_mapping_dict,
-                                    random_crop=RandomCrop(CROP_SIZE),
-                                    mean=MEAN, std=STD)
+# ds = lcm.DataSetPatches(im_dir=dir_test_im_patches, mask_dir=dir_test_mask_patches, 
+#                                     mask_suffix=mask_suffix_test_ds, mask_dir_name=mask_dir_name_test,
+#                                 #   list_tile_names=dict_tile_names_sample['test'],
+#                                     list_tile_patches_use=None,
+#                                     shuffle_order_patches=True, relabel_masks=True,
+#                                     random_transform_data=True,
+#                                     subsample_patches=False, # frac_subsample=0.1,
+#                                     path_mapping_dict=path_mapping_dict,
+#                                     random_crop=RandomCrop(CROP_SIZE),
+#                                     mean=MEAN, std=STD)
 
-M = image.shape[0]//16
-N = image.shape[1]//16
+DATA_PATH = f'{ROOT_PATH}/data/example_tiles_complete/12.5cm-Aerial-Photo'
 
-tiles = [image[x:x+M,y:y+N] for x in range(0,image.shape[0],M) for y in range(0,image.shape[1],N)]
-tiles2 = [image2[x:x+M,y:y+N] for x in range(0,image2.shape[0],M) for y in range(0,image2.shape[1],N)]
+path1 = f'{DATA_PATH}/2010-2017/119119-2_RGB_25_Shape/SK0961.tif'
+path2 = f'{DATA_PATH}/2020-2021/119120-1_RGB_25_Shape/SK0961.tif'
+
+image = torch.tensor(imageio.v2.imread(path1))
+image2 = torch.tensor(imageio.v2.imread(path2))
+
+image = image[:-64,:-64,:]
+image2 = image2[:-64,:-64,:]
+
+M = image.shape[0]//62
+N = image.shape[1]//62
+
+tiles  = torch.stack([image[x:x+M,y:y+N] for x in range(0,image.shape[0],M) for y in range(0,image.shape[1],N)])
+tiles2 = torch.stack([image2[x:x+M,y:y+N] for x in range(0,image2.shape[0],M) for y in range(0,image2.shape[1],N)])
 # tiles3 = [image3[x:x+M,y:y+N] for x in range(0,image3.shape[0],M) for y in range(0,image3.shape[1],N)]
 
+tiles = tiles.permute(0,3,1,2)
+tiles2 = tiles2.permute(0,3,1,2)
 
-length = len(ds)
 
-# Do the train-test split
-perm = torch.randperm(len(ds))
+# length = len(ds)
 
-train_ds = torch.utils.data.Subset(ds, perm[:int(TRAIN_TEST_RATIO*length)])
-val_ds = torch.utils.data.Subset(ds, perm[int(TRAIN_TEST_RATIO*length):])
+# # Do the train-test split
+# perm = torch.randperm(len(ds))
+
+# train_ds = torch.utils.data.Subset(ds, perm[:int(TRAIN_TEST_RATIO*length)])
+# val_ds = torch.utils.data.Subset(ds, perm[int(TRAIN_TEST_RATIO*length):])
+
+
 
 # Define dataloaders
-trainloader = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, pin_memory=True,
-                                          shuffle=True, num_workers=2)
-valloader = torch.utils.data.DataLoader(val_ds, batch_size=BATCH_SIZE, pin_memory=True,
-                                          shuffle=True, num_workers=2)
+# trainloader = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, pin_memory=True,
+#                                           shuffle=True, num_workers=2)
+# valloader = torch.utils.data.DataLoader(val_ds, batch_size=BATCH_SIZE, pin_memory=True,
+                                        #   shuffle=True, num_workers=2)
 
 
 torch.manual_seed(SEED)
 
-# if torch.cuda.is_available():
-#     device = torch.device("cuda")
-# else:
-device = torch.device("cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 
 # Train the model
 
 # model = SiameseNetwork(output_dim=(1,CROP_SIZE*CROP_SIZE)).to(device)
 # model = SNUNet_ECAM(3, 1).to(device)
+
+# model = torch.load(f'{ROOT_PATH}/model_100.pth', map_location=torch.device('cpu'))
+# print(model)
 model = UNet(n_channels=3, n_classes=1).to(device)
-model.load_state_dict(torch.load('/shared/miguel/peak-district-challenge/results_inclass_mixing/model_40.pth'))
+if device == torch.device("cuda"):
+    model.load_state_dict(torch.load(f'{ROOT_PATH}/results_inclass_mixing/model_100.pth'))
+else:
+    model.load_state_dict(torch.load(f'{ROOT_PATH}/results_inclass_mixing/model_100.pth'), map_location=torch.device('cpu'))
+
 model.eval()
 
 criterion = nn.MSELoss()#reduction='sum')
 
-iterator = iter(valloader)
-batch = next(iterator)
-train_correct = 0
-train_cum_loss = []
+for i, (im, im2) in enumerate(zip(tiles, tiles2)):
+    
+    # train_correct = 0
+    # train_cum_loss = []
 
-original_image, augmented_image, change_mask, mixing_img, fouriermask, class_change = combine_masks_prediction(batch, "continuous_fmix")
-        
-change_mask = change_mask.float()
+    # batch = torch.stack([im, im2])
+    # print(batch.shape)
 
-original_image_transformed = extra_transforms(original_image)
-augmented_image_transformed = extra_transforms(augmented_image)
+    # original_image, augmented_image, change_mask, mixing_img, fouriermask, class_change = combine_masks_prediction(batch, "continuous_fmix")
+            
+    # change_mask = change_mask.float()
 
-# Send to CUDA device
-original_image = original_image.to(device)
-augmented_image = augmented_image.to(device)
-change_mask = change_mask.to(device)
-        
-outputs = model(original_image.float(), augmented_image.float())
-loss = criterion(outputs.squeeze(), change_mask)
-        
-pred = torch.where(outputs > 0.5, 1, 0)  # get the index of the max log-probability
-change_mask = torch.where(change_mask > 0.5, 1, 0)
-train_correct += pred.eq(change_mask.view_as(pred)).sum().item()
+    # original_image_transformed = extra_transforms(original_image)
+    # augmented_image_transformed = extra_transforms(augmented_image)
 
-train_cum_loss.append(loss.sum().item())  # cumulative train loss
+    # Send to CUDA device
+    original_image = im.unsqueeze(0).to(device)
+    augmented_image = im2.unsqueeze(0).to(device)
+    # change_mask = change_mask.to(device)
+            
+    outputs = model(original_image.float(), augmented_image.float())
+    # loss = criterion(outputs.squeeze(), change_mask)
+            
+    pred = torch.where(outputs > 0.5, 1, 0)  # get the index of the max log-probability
+    # change_mask = torch.where(change_mask > 0.5, 1, 0)
+    # train_correct += pred.eq(change_mask.view_as(pred)).sum().item()
 
-train_loss = sum(train_cum_loss) / len(trainloader.dataset)
-train_correct /= len(trainloader.dataset)
-train_correct /= CROP_SIZE*CROP_SIZE
-train_acc = 100. * train_correct
+    # train_cum_loss.append(loss.sum().item())  # cumulative train loss
+
+    # train_loss = sum(train_cum_loss) / len(tiles)
+    # train_correct /= len(tiles)
+    # train_correct /= CROP_SIZE*CROP_SIZE
+    # train_acc = 100. * train_correct
 
 
-print("Accuracy eval:", train_acc)
+    # print("Accuracy eval:", train_acc)
 
 
-import matplotlib.pyplot as plt
 
-i=0
-        
-# Plot the original image and mask
-fig, ax = plt.subplots(3,3, figsize=(10,10))
-ax[0][0].set_title('Original image 1')
-ax[0][0].imshow(original_image[i].permute(1,2,0))#.type(torch.uint8))#, vmin=0, vmax=1)
-ax[0][1].set_title('Mixing with image 2')
-ax[0][1].imshow(mixing_img[i].permute(1,2,0))#.type(torch.uint8))#, vmin=0, vmax=1)
-ax[0][2].set_title('Fourier generated mask')
-ax[0][2].imshow(fouriermask[i], vmin=0, vmax=1)
-# ax[0][1].imshow(batch[1][i])
-# io.imshow(color.label2rgb(batch[1][i],batch[0][i].permute(1,2,0)))
-# ax[0][1].set_title('Original mask 1')
+    if PLOT:
+        import matplotlib.pyplot as plt
 
-ax[1][0].set_title('Augmentations image 1')
-ax[1][0].imshow(original_image_transformed[i].permute(1,2,0))
-ax[1][1].set_title('Augmentations image 2')
-ax[1][1].imshow(augmented_image_transformed[i].permute(1,2,0))
-ax[1][2].set_title('Class change')
-ax[1][2].imshow(class_change[i])
+        i=0
+                
+        # Plot the original image and mask
+        fig, ax = plt.subplots(2,2, figsize=(10,10))
+        ax[0][0].set_title('Image 1')
+        ax[0][0].imshow(im.permute(1,2,0))#.type(torch.uint8))#, vmin=0, vmax=1)
+        ax[0][1].set_title('Image 2')
+        ax[0][1].imshow(im2.permute(1,2,0))#.type(torch.uint8))#, vmin=0, vmax=1)
+        # ax[0][2].set_title('Fourier generated mask')
+        # ax[0][2].imshow(fouriermask[i], vmin=0, vmax=1)
+        # ax[0][1].imshow(batch[1][i])
+        # io.imshow(color.label2rgb(batch[1][i],batch[0][i].permute(1,2,0)))
+        # ax[0][1].set_title('Original mask 1')
 
-ax[2][0].set_title('Change mask between 1 and 2 (GT)')
-ax[2][0].imshow(change_mask[i], vmin=0, vmax=1)
-ax[2][1].set_title('Predicted change')
-ax[2][1].imshow(outputs[i].permute(1,2,0).detach().numpy())
-ax[2][2].set_title('Predicted change between 0 and 1')
-ax[2][2].imshow(outputs[i].permute(1,2,0).detach().numpy(), vmin=0, vmax=1)
+        # ax[1][0].set_title('Augmentations image 1')
+        # ax[1][0].imshow(original_image_transformed[i].permute(1,2,0))
+        # ax[1][1].set_title('Augmentations image 2')
+        # ax[1][1].imshow(augmented_image_transformed[i].permute(1,2,0))
+        # ax[1][2].set_title('Class change')
+        # ax[1][2].imshow(class_change[i])
 
-print(outputs[i].max())
-print(outputs[i].min())
-print('----')
-print(change_mask[i].max())
-print(change_mask[i].min())
-plt.show()
+        # ax[2][0].set_title('Change mask between 1 and 2 (GT)')
+        # ax[2][0].imshow(change_mask[i], vmin=0, vmax=1)
+        ax[1][1].set_title('Predicted change')
+        ax[1][1].imshow(outputs[i].permute(1,2,0).detach().cpu().numpy())
+        ax[1][2].set_title('Predicted change between 0 and 1')
+        ax[1][2].imshow(outputs[i].permute(1,2,0).detach().cpu().numpy(), vmin=0, vmax=1)
+
+        # print(outputs[i].max())
+        # print(outputs[i].min())
+        # print('----')
+        # print(change_mask[i].max())
+        # print(change_mask[i].min())
+        plt.show()
